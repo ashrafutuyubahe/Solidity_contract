206b3c59bf50de324947a925565d8bab2927c5cf
ashrafutuyubahe <ashraftuyubahe001@gmail.com>
2025-01-10 15:12:01 +0200

    added hello world program in the contract

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -9,8 +9,26 @@ pragma solidity >=0.8.2 <0.9.0;
  */
 contract Storage {
 
-    function sayHelloWorld() public pure returns (string memory) {
-        return "Hello World";
+    uint256 number;
+
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function store(uint256 num) public {
+        number = num;
     }
+
+
+
+
+    /**
+     * @dev Return value 
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+    
     
 }
